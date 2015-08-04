4931fba0f6b760858f97b27ddc3c227f2e8abf2c
Wire55 <seemetrollinlol@gmail.com>
2015-08-03 21:06:03 -0500

    Update millet-common-vendor-blobs.mk

--- millet-common-vendor-blobs.mk
+++ millet-common-vendor-blobs.mk
@@ -143,7 +143,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/vendor/lib/C08QT_libchromatix_imx175_common_res1.so:system/vendor/lib/C08QT_libchromatix_imx175_common_res1.so \
     $(LOCAL_PATH)/proprietary/vendor/lib/C08QT_libchromatix_imx175_default_video.so:system/vendor/lib/C08QT_libchromatix_imx175_default_video.so \
     $(LOCAL_PATH)/proprietary/vendor/lib/C08QT_libchromatix_imx175_liveshot.so:system/vendor/lib/C08QT_libchromatix_imx175_liveshot.so \
-    $(LOCAL_PATH)/proprietary/vendor/lib/C08QT_libchromatix_imx175_panorama.so:system/vendor/lib/C08QT_libchromatix_imx175_panorama.so \
     $(LOCAL_PATH)/proprietary/vendor/lib/C08QT_libchromatix_imx175_preview.so:system/vendor/lib/C08QT_libchromatix_imx175_preview.so \
     $(LOCAL_PATH)/proprietary/vendor/lib/C08QT_libchromatix_imx175_snapshot.so:system/vendor/lib/C08QT_libchromatix_imx175_snapshot.so \
     $(LOCAL_PATH)/proprietary/vendor/lib/C08QT_libchromatix_imx175_video_hd.so:system/vendor/lib/C08QT_libchromatix_imx175_video_hd.so \
